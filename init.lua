ccd14c8a272eb98a9d51c563a19983f062c202b6
Mikhail Morozov <inferst@gmail.com>
2024-07-08 23:40:16 +0300

    config

--- init.lua
+++ init.lua
@@ -1004,11 +1004,11 @@ require('lazy').setup({
         --  If you are experiencing weird indenting issues, add the language to
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
         additional_vim_regex_highlighting = { 'ruby' },
-        disable = function(_, bufnr)
-          local buf_name = vim.api.nvim_buf_get_name(bufnr)
-          local file_size = vim.api.nvim_call_function('getfsize', { buf_name })
-          return file_size > 256 * 1024
-        end,
+        -- disable = function(_, bufnr)
+        --   local buf_name = vim.api.nvim_buf_get_name(bufnr)
+        --   local file_size = vim.api.nvim_call_function('getfsize', { buf_name })
+        --   return file_size > 256 * 1024
+        -- end,
       },
       indent = { enable = true, disable = { 'ruby' } },
       textobjects = {
