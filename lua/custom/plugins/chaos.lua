3a70e2b23ce3f401c4e8590db8d8ddedb11348ae
Mikhail Morozov <inferst@gmail.com>
2024-07-31 14:24:38 +0300

    config

--- lua/custom/plugins/chaos.lua
+++ lua/custom/plugins/chaos.lua
@@ -1,6 +1,6 @@
 return {
   'inferst/nvim-chaos',
-  dir = '/home/inferst/projects/nvim-chaos/',
+  -- dir = '/home/inferst/projects/nvim-chaos/',
   build = './install.sh release',
   config = function(_)
     local plugin = require 'nvim_chaos'
